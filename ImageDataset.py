a52e0bff26638d09d76590cc94d8a991a328008d
MerelMM <48050464+MerelMM@users.noreply.github.com>
2024-04-03 10:12:18 +0200

    Add files via upload
    still working on it, but thought i put it already on here as intermediate result

---
+++ ImageDataset.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from PIL import Image
+from torch.utils.data import Dataset
+import os
+
+class ImageDataset(Dataset):
+    def __init__(self, data, root_dir, transform=None):
+        super().__init__()
+        self.data = data
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img_name = self.data.iloc[idx, 1]
+        img_path = os.path.join(self.root_dir, img_name)
+        image = Image.open(img_path)
+        label = self.data.iloc[idx, 0]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
