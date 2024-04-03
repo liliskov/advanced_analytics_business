cf0ce34621192a6030504691598fe85f5aa479e5
MerelMM <merel@live.be>
2024-04-03 13:32:37 +0200

    other dim

--- assignment 2/Net.py
+++ assignment 2/Net.py
@@ -3,17 +3,19 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.feature_extractor = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3,padding =1),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(3, 64, kernel_size=3,padding =1),
+            nn.BatchNorm2d(64),
             nn.ELU(),
             nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(32,32, kernel_size=3,padding =1),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(64,64, kernel_size=3,padding =1),
+            nn.BatchNorm2d(64),
             nn.ELU(),
-            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(64,64, kernel_size=3,padding =1),
+            nn.BatchNorm2d(64),
+            nn.ELU(),
             nn.Flatten()
         )
-        self.regressor = nn.Linear(64 * 16 * 16, 1)
+        self.regressor = nn.Linear(2359296, 1)
 
 
 
